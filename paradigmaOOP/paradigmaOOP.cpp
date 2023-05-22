12affce753898951f039cd02530fee6b3d565b6d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 19:41:04 +0700

    s

--- paradigmaOOP/paradigmaOOP.cpp
+++ paradigmaOOP/paradigmaOOP.cpp
@@ -12,4 +12,30 @@ public:
 	~orang() {
 		cout << "Orang dihapus\n" << endl;
 	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa1("Muhammad Rezza", "SMAN 1 BEKASI");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
