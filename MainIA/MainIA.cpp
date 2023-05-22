2802eab672f18da73f97ae8df018c96b500621d1
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 16:00:48 +0700

    dd

--- MainIA/MainIA.cpp
+++ MainIA/MainIA.cpp
@@ -4,4 +4,17 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
-int main ()
+int main() {
+	ibu* varIbu = new ibu("Ratih");
+	ibu* varIbu2 = new ibu("Shella");
+	anak* varAnak1 = new anak("Rasya");
+	anak* varAnak2 = new anak("Rezza");
+	anak* varAnak3 = new anak("Nandu");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+
+}
