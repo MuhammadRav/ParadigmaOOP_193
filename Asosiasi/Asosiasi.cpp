17c7723e77152caa9936a8c0dd8cfe4b1f43c24b
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 20:26:46 +0700

    memperbaiki

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -3,36 +3,43 @@
 using namespace std;
 
 class dokter;
-	class pasien {
-	public:
-		string nama;
-		vector<dokter*> daftar_dokter;
-		pasien(string pNama) :nama(pNama) {
-			cout << "Pasien\"" << nama << "\" tidak ada\n";
-		}
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
 
-		void tambahDokter(dokter*);
-		void cetakDokter();
+	void tambahDokter(dokter*);
+	void cetakDokter();
 };
 
 class dokter {
-	public:
-		string nama;
-		vector<pasien*> daftar_pasien;
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
 
-		dokter(string pNama) :nama(pNama) {
-			cout << "Dokter \"" << nama << "\" ada\n";
-		}
-		~dokter() {
-			cout << "Dokter \"" << nama << "\" tidak ada\n";
-		}
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
 
-		void tambahPasien(pasien*);
-		void cetakPasien();
-	};
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
 
 void pasien::cetakDokter() {
-	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	cout << "\nDaftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
 	for (auto& a : daftar_dokter) {
 		cout << a->nama << "\n";
 	}
@@ -43,7 +50,7 @@ void dokter::tambahPasien(pasien* pPasien) {
 	pPasien->tambahDokter(this);
 }
 void dokter::cetakPasien() {
-	cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+	cout << "\nDaftar Pasien dari dokter \"" << this->nama << "\":\n";
 	for (auto& a : daftar_pasien) {
 		cout << a->nama << "\n";
 	}
