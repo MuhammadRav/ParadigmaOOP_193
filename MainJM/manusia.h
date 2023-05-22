fc969fe3434df456cb6138403e6ac5b1c770c7a4
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 19:57:41 +0700

    ss

---
+++ MainJM/manusia.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName) :name(pName) {
+		cout << name << "Hidup\n";
+	}
+	~manusia(){
+		cout << name << "Mati\n";
+	}
+};
+#endif
