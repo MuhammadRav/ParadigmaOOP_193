acf2316d183c30e491a5e32353869c0b4413bf9d
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 15:44:01 +0700

    ww

---
+++ MainIA/ibu.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada \n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" ada \n";
+	}
+	
