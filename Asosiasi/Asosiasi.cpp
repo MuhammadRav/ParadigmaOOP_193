d9e3b8f18b1b86c381a5557645490a7fa09c2717
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 20:03:45 +0700

    dd

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -13,5 +13,21 @@ class dokter;
 
 		void tambahDokter(dokter*);
 		void cetakDoketr();
-
 };
+
+class dokter {
+	public:
+		string nama;
+		vector<pasien*> daftar_pasien;
+
+		dokter(string pNama) :nama(pNama) {
+			cout << "Dokter \"" << nama << "\" ada\n";
+		}
+		~dokter() {
+			cout << "Dokter \"" << nama << "\" tidak ada\n";
+		}
+
+		void tambahPasien(pasien*);
+		void cetakPasien();
+	};
+
