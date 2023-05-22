502dfc86ad98c4eda07ed2ca035d9f20804e4017
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-05-22 19:35:06 +0700

    s

---
+++ paradigmaOOP/paradigmaOOP.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapus\n" << endl;
+	}
+};
